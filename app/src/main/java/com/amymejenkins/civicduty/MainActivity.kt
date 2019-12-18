d5c9c013ee3b7eab7d4393dfb1e942e3bcd09f8a
Amy Jenkins <57229385+amyejenkins@users.noreply.github.com>
2019-12-18 15:18:38 -0500

    Open new activity on btn click

--- app/src/main/java/com/amymejenkins/civicduty/MainActivity.kt
+++ app/src/main/java/com/amymejenkins/civicduty/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.amymejenkins.civicduty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
-//const val ADDRESS = "com.amymejenkins.civicduty.ADDRESS"
+const val ADDRESS = "com.amymejenkins.civicduty.ADDRESS"
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +24,10 @@ class MainActivity : AppCompatActivity() {
 
     fun editText(view: View) {
         var addressTextView: TextView = findViewById(R.id.address_text)
-        addressTextView.text = "new text"
+        val address = addressTextView.text.toString()
+        val intent = Intent(this, EditAddressActivity::class.java).apply {
+            putExtra(ADDRESS, address)
+        }
+        startActivity(intent)
     }
 }
