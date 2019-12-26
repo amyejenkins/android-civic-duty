3ef56a1ca5a4f5fcfc91e81fc17698bc0903b80d
Amy Jenkins <57229385+amyejenkins@users.noreply.github.com>
2019-12-26 14:58:56 -0500

    add placeholder alert fragment

---
+++ app/src/main/java/com/amymejenkins/civicduty/ErrorDialogFragment.kt
@@ -0,0 +1,27 @@
+package com.amymejenkins.civicduty
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+const val MESSAGE = "MESSAGE"
+
+class ErrorDialogFragment : DialogFragment() {
+    var errorMessage = R.string.error_message
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        super.onCreateDialog(savedInstanceState)
+
+        return activity?.let {
+            val message = arguments?.getInt(MESSAGE) ?: R.string.error_message;
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage(message)
+                .setPositiveButton(R.string.action_ok,
+                    DialogInterface.OnClickListener { dialog, id ->
+                    })
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
