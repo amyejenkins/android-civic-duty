9384eafc796050d324e5a832b1ff5ab91825df2b
Amy Jenkins <57229385+amyejenkins@users.noreply.github.com>
2019-12-27 09:14:18 -0500

    Create about page and link to it in overflow menu

--- app/src/main/java/com/amymejenkins/civicduty/DisplayFragment.kt
+++ app/src/main/java/com/amymejenkins/civicduty/DisplayFragment.kt
@@ -1,13 +1,13 @@
 package com.amymejenkins.civicduty
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.amymejenkins.civicduty.databinding.FragmentDisplayBinding
 
 class DisplayFragment : Fragment() {
@@ -23,6 +23,22 @@ class DisplayFragment : Fragment() {
 
         binding.addressText.text = vm.address
 
+        // tell Android that our TitleFragment has a menu
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    // inflate our new menu resource
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    // If NavigationUI handles the selection, go that route; else directly handle the menu item
+    // without navigating
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
